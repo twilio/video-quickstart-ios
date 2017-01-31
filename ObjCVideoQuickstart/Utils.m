5124d6cb9eccadde1f09c72f6762fe6897b1352e
Ryan C. Payne <rpayne@twilio.com>
2017-01-31 15:22:15 -0500

    Updated copyright information.

--- ObjCVideoQuickstart/Utils.m
+++ ObjCVideoQuickstart/Utils.m
@@ -2,7 +2,7 @@
 //  Utils.m
 //  ObjCVideoQuickstart
 //
-//  Copyright © 2016 Twilio, Inc. All rights reserved.
+//  Copyright © 2016-2017 Twilio, Inc. All rights reserved.
 //
 
 #import "Utils.h"
