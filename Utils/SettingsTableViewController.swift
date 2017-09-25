e37dfb6c72881ac4368a1b269ca181f839b4c1c3
Piyush Tank <ptank@twilio.com>
2017-09-25 12:50:42 -0700

    Adding encoding parameter settings

--- Utils/SettingsTableViewController.swift
+++ Utils/SettingsTableViewController.swift
@@ -12,10 +12,18 @@ class SettingsTableViewController: UITableViewController {
     
     static let audioCodecLabel = "Audio Codec"
     static let videoCodecLabel = "Video Codec"
-    static let defaultCodecStr = "Default"
+    static let maxAudioBitrate = "Max Audio Bitrate"
+    static let maxVideoBitrate = "Max Video Bitrate"
+    static let defaultStr = "Default"
+    static let codecSectionTitle = "Codecs"
+    static let encodingParamSectionTitle = "Encoding Parameters"
     static let disclaimerText = "Set your preferred audio and video codec. Not all codecs are supported with Group rooms. The media server will fallback to OPUS or VP8 if a preferred codec is not supported."
     
-    let labels: [String] = [SettingsTableViewController.audioCodecLabel, SettingsTableViewController.videoCodecLabel]
+    let labels: [String] = [SettingsTableViewController.audioCodecLabel,
+                            SettingsTableViewController.videoCodecLabel,
+                            SettingsTableViewController.maxAudioBitrate,
+                            SettingsTableViewController.maxVideoBitrate]
+    
     let settings = Settings.shared
     let disclaimerFont = UIFont.preferredFont(forTextStyle: UIFontTextStyle.footnote)
 
@@ -48,104 +56,59 @@ class SettingsTableViewController: UITableViewController {
         // Configure the cell...
         let label = self.labels[indexPath.row]
         cell.textLabel?.text = label
-        let settings = Settings.shared
-
+        var detailedText = SettingsTableViewController.defaultStr
+        
         switch (label) {
             case SettingsTableViewController.audioCodecLabel:
-                var codecStr = SettingsTableViewController.defaultCodecStr
                 if let codec = settings.audioCodec {
-                    codecStr = codec.rawValue
+                    detailedText = codec.rawValue
                 }
-                cell.detailTextLabel?.text = codecStr
                 break;
             
             case SettingsTableViewController.videoCodecLabel:
-                var codecStr = SettingsTableViewController.defaultCodecStr
                 if let codec = settings.videoCodec {
-                    codecStr = codec.rawValue
+                    detailedText = codec.rawValue
                 }
-                cell.detailTextLabel?.text = codecStr
+                break;
+            
+            case SettingsTableViewController.maxAudioBitrate:
+                detailedText = String(settings.maxAudioBitrate)
+                break;
+            
+            case SettingsTableViewController.maxVideoBitrate:
+                detailedText = String(settings.maxVideoBitrate)
                 break;
 
             default:
                 break;
         }
-
+        cell.detailTextLabel?.text = detailedText
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let tappedLabel = self.labels[indexPath.row]
         
-        let alertController = UIAlertController(title: self.labels[indexPath.row], message: nil, preferredStyle: .actionSheet)
-        var selectedButton : UIAlertAction!
-        var defaultButton: UIAlertAction!
-        
         switch (tappedLabel) {
             case SettingsTableViewController.audioCodecLabel:
-                let selectionArray = settings.supportedAudioCodecs
-                
-                for codec in selectionArray {
-                    let selectionButton = UIAlertAction(title: codec.rawValue, style: .default, handler: { (action) -> Void in
-                        self.settings.audioCodec = codec
-                        self.tableView.reloadData()
-                    })
-                    
-                    if (settings.audioCodec == codec) {
-                        selectedButton = selectionButton;
-                    }
-                    
-                    alertController.addAction(selectionButton)
-                }
-                
-                defaultButton = UIAlertAction(title: "Default", style: .default, handler: { (action) -> Void in
-                    self.settings.audioCodec = nil
-                    self.tableView.reloadData()
-                })
+                didSelectAudioCodecRow(indexPath: indexPath)
                 break;
 
             case SettingsTableViewController.videoCodecLabel:
-                let selectionArray = settings.supportedVideoCodecs
-                
-                for codec in selectionArray {
-                    let selectionButton = UIAlertAction(title: codec.rawValue, style: .default, handler: { (action) -> Void in
-                        self.settings.videoCodec = codec
-                        self.tableView.reloadData()
-                    })
-                    
-                    if (settings.videoCodec == codec) {
-                        selectedButton = selectionButton;
-                    }
-
-                    alertController.addAction(selectionButton)
-                }
-                
-                defaultButton = UIAlertAction(title: "Default", style: .default, handler: { (action) -> Void in
-                    self.settings.videoCodec = nil
-                    self.tableView.reloadData()
-                })
+                didSelectVideoCodecRow(indexPath: indexPath)
                 break;
+
+            case SettingsTableViewController.maxAudioBitrate:
+                didSelectMaxAudioBitRateRow(indexPath: indexPath)
+                break
+            
+            case SettingsTableViewController.maxVideoBitrate:
+                didSelectMaxVideoBitRateRow(indexPath: indexPath)
+                break
             
             default:
                 break;
         }
-        
-        if selectedButton == nil {
-            selectedButton = defaultButton;
-        }
-        
-        alertController.addAction(defaultButton!)
-        
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            alertController.popoverPresentationController?.sourceView = tableView.cellForRow(at: indexPath)
-            alertController.popoverPresentationController?.sourceRect = (tableView.cellForRow(at: indexPath)?.bounds)!
-        } else {
-            let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in })
-            alertController.addAction(cancelButton)
-            selectedButton!.setValue("true", forKey: "checked")
-        }
-
-        self.navigationController!.present(alertController, animated: true, completion: nil)
     }
     
     override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
@@ -164,5 +127,149 @@ class SettingsTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return self.getDisclaimerSize().height
+    }
+    
+    func didSelectAudioCodecRow(indexPath: IndexPath) {
+        var selectedButton : UIAlertAction!
+        var defaultButton: UIAlertAction!
+        
+        let alertController = UIAlertController(title: self.labels[indexPath.row], message: nil, preferredStyle: .actionSheet)
+        let selectionArray = settings.supportedAudioCodecs
+        
+        for codec in selectionArray {
+            let selectionButton = UIAlertAction(title: codec.rawValue, style: .default, handler: { (action) -> Void in
+                self.settings.audioCodec = codec
+                self.tableView.reloadData()
+            })
+            
+            if (settings.audioCodec == codec) {
+                selectedButton = selectionButton;
+            }
+            
+            alertController.addAction(selectionButton)
+        }
+        
+        // The default action
+        defaultButton = UIAlertAction(title: "Default", style: .default, handler: { (action) -> Void in
+            self.settings.audioCodec = nil
+            self.tableView.reloadData()
+        })
+        
+        if selectedButton == nil {
+            selectedButton = defaultButton;
+        }
+        
+        alertController.addAction(defaultButton!)
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            alertController.popoverPresentationController?.sourceView = tableView.cellForRow(at: indexPath)
+            alertController.popoverPresentationController?.sourceRect = (tableView.cellForRow(at: indexPath)?.bounds)!
+        } else {
+            selectedButton?.setValue("true", forKey: "checked")
+            
+            // Adding the cancel action
+            let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in })
+            alertController.addAction(cancelButton)
+        }
+        self.navigationController!.present(alertController, animated: true, completion: nil)
+    }
+    
+    func didSelectVideoCodecRow(indexPath: IndexPath) {
+        var selectedButton : UIAlertAction!
+        
+        let defaultButton = UIAlertAction(title: "Default", style: .default, handler: { (action) -> Void in
+            self.settings.videoCodec = nil
+            self.tableView.reloadData()
+        })
+        
+        let alertController = UIAlertController(title: self.labels[indexPath.row], message: nil, preferredStyle: .actionSheet)
+        let selectionArray = settings.supportedVideoCodecs
+        
+        for codec in selectionArray {
+            let selectionButton = UIAlertAction(title: codec.rawValue, style: .default, handler: { (action) -> Void in
+                self.settings.videoCodec = codec
+                self.tableView.reloadData()
+            })
+            
+            if (settings.videoCodec == codec) {
+                selectedButton = selectionButton;
+            }
+            
+            alertController.addAction(selectionButton)
+        }
+        
+        if selectedButton == nil {
+            selectedButton = defaultButton;
+        }
+        
+        alertController.addAction(defaultButton)
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            alertController.popoverPresentationController?.sourceView = tableView.cellForRow(at: indexPath)
+            alertController.popoverPresentationController?.sourceRect = (tableView.cellForRow(at: indexPath)?.bounds)!
+        } else {
+            selectedButton?.setValue("true", forKey: "checked")
+            
+            // Adding the cancel action
+            let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in })
+            alertController.addAction(cancelButton)
+        }
+        self.navigationController!.present(alertController, animated: true, completion: nil)
+    }
+    
+    func didSelectMaxAudioBitRateRow(indexPath: IndexPath) {
+        let alertController = UIAlertController(title: self.labels[indexPath.row], message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField  { (textField : UITextField!) -> Void in
+            textField.text = String(self.settings.maxAudioBitrate)
+            textField.placeholder = "Max audio bitrate"
+            textField.keyboardType = .numberPad
+        }
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { alert -> Void in
+            let maxAudioBitrate = alertController.textFields![0] as UITextField
+            if maxAudioBitrate.text! != "" {
+                self.settings.maxAudioBitrate = UInt(maxAudioBitrate.text!)!
+                self.tableView.reloadData()
+            }
+        })
+        alertController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in })
+        alertController.addAction(cancelAction)
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            alertController.popoverPresentationController?.sourceView = tableView.cellForRow(at: indexPath)
+            alertController.popoverPresentationController?.sourceRect = (tableView.cellForRow(at: indexPath)?.bounds)!
+        }
+        self.navigationController!.present(alertController, animated: true, completion: nil)
+    }
+    
+    func didSelectMaxVideoBitRateRow(indexPath: IndexPath) {
+        let alertController = UIAlertController(title: self.labels[indexPath.row], message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField  { (textField : UITextField!) -> Void in
+            textField.text = String(self.settings.maxVideoBitrate)
+            textField.placeholder = "Max video bitrate"
+            textField.keyboardType = .numberPad
+        }
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { alert -> Void in
+            let maxVideoBitrate = alertController.textFields![0] as UITextField
+            if maxVideoBitrate.text! != "" {
+                self.settings.maxVideoBitrate = UInt(maxVideoBitrate.text!)!
+                self.tableView.reloadData()
+            }
+        })
+        alertController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in })
+        alertController.addAction(cancelAction)
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            alertController.popoverPresentationController?.sourceView = tableView.cellForRow(at: indexPath)
+            alertController.popoverPresentationController?.sourceRect = (tableView.cellForRow(at: indexPath)?.bounds)!
+        }
+        self.navigationController!.present(alertController, animated: true, completion: nil)
     }
 }
