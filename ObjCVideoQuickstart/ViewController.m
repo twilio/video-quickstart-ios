28bdca5809d2bafabc1d79e66f832835fbf4f02c
Piyush Tank <ptank@twilio.com>
2017-11-03 10:06:08 -0700

    Using track names in basic quickstart (#90)
    * Using track names in basic quickstart
    
    * Adjusting fantasize to acomodate text in message label per review feedbacks

--- ObjCVideoQuickstart/ViewController.m
+++ ObjCVideoQuickstart/ViewController.m
@@ -54,7 +54,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.title = @"QuickStart";
+    self.messageLabel.adjustsFontSizeToFitWidth = YES;
+    self.messageLabel.minimumScaleFactor = 0.75;
 
     [self logMessage:[NSString stringWithFormat:@"TwilioVideo v%@", [TwilioVideo version]]];
 
@@ -157,7 +160,10 @@
     }
     
     self.camera = [[TVICameraCapturer alloc] initWithSource:TVICameraCaptureSourceFrontCamera delegate:self];
-    self.localVideoTrack = [TVILocalVideoTrack trackWithCapturer:self.camera];
+    self.localVideoTrack = [TVILocalVideoTrack trackWithCapturer:self.camera
+                                                         enabled:YES
+                                                     constraints:nil
+                                                            name:@"Camera"];
     if (!self.localVideoTrack) {
         [self logMessage:@"Failed to add video track"];
     } else {
@@ -186,7 +192,9 @@
     
     // Create an audio track.
     if (!self.localAudioTrack) {
-        self.localAudioTrack = [TVILocalAudioTrack track];
+        self.localAudioTrack = [TVILocalAudioTrack trackWithOptions:nil
+                                                            enabled:YES
+                                                               name:@"Microphone"];
 
         if (!self.localAudioTrack) {
             [self logMessage:@"Failed to add audio track"];
@@ -200,7 +208,7 @@
 
     // Create a data track which will be used to share location information
     if (!self.localDataTrack) {
-        self.localDataTrack = [TVILocalDataTrack track];
+        self.localDataTrack = [TVILocalDataTrack trackWithOptions:nil name:@"Location"];
     }
 }
 
@@ -388,7 +396,8 @@
     
     // Remote Participant has offered to share the video Track.
 
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ published video track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ published %@ video track .",
+                      participant.identity, publication.trackName]];
 }
 
 - (void)remoteParticipant:(TVIRemoteParticipant *)participant
@@ -396,7 +405,8 @@
     
     // Remote Participant has stopped sharing the video Track.
     
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ unpublished video track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ unpublished %@ video track.",
+                      participant.identity, publication.trackName]];
 }
 
 - (void)remoteParticipant:(TVIRemoteParticipant *)participant
@@ -404,7 +414,8 @@
     
     // Remote Participant has offered to share the audio Track.
     
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ published audio track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ published %@ audio track.",
+                      participant.identity, publication.trackName]];
 }
 
 - (void)remoteParticipant:(TVIRemoteParticipant *)participant
@@ -412,7 +423,8 @@
     
     // Remote Participant has stopped sharing the audio Track.
     
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ unpublished audio track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ unpublished %@ audio track.",
+                      participant.identity, publication.trackName]];
 }
 
 - (void)remoteParticipant:(TVIRemoteParticipant *)participant
@@ -420,7 +432,8 @@
 
     // Remote Participant has offered to share the data Track.
 
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ published data track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ published %@ data track.",
+                      participant.identity, publication.trackName]];
 }
 
 - (void)remoteParticipant:(TVIRemoteParticipant *)participant
@@ -428,7 +441,8 @@
 
     // Remote Participant has stopped sharing the data Track.
 
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ unpublished data track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ unpublished %@ data track.",
+                      participant.identity, publication.trackName]];
 }
 
 - (void)subscribedToVideoTrack:(TVIRemoteVideoTrack *)videoTrack
@@ -438,7 +452,8 @@
     // We are subscribed to the remote Participant's audio Track. We will start receiving the
     // remote Participant's video frames now.
     
-    [self logMessage:[NSString stringWithFormat:@"Subscribed to video track for Participant %@", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Subscribed to %@ video track for Participant %@",
+                      publication.trackName, participant.identity]];
     
     if (self.remoteParticipant == participant) {
         [self setupRemoteView];
@@ -453,7 +468,8 @@
     // We are unsubscribed from the remote Participant's video Track. We will no longer receive the
     // remote Participant's video.
     
-    [self logMessage:[NSString stringWithFormat:@"Unsubscribed from video track for Participant %@", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Unsubscribed from %@ video track for Participant %@",
+                      publication.trackName, participant.identity]];
     
     if (self.remoteParticipant == participant) {
         [videoTrack removeRenderer:self.remoteView];
@@ -468,7 +484,8 @@
     // We are subscribed to the remote Participant's audio Track. We will start receiving the
     // remote Participant's audio now.
     
-    [self logMessage:[NSString stringWithFormat:@"Subscribed to audio track for Participant %@", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Subscribed to %@ audio track for Participant %@",
+                      publication.trackName, participant.identity]];
 }
 
 - (void)unsubscribedFromAudioTrack:(TVIRemoteAudioTrack *)audioTrack
@@ -478,7 +495,8 @@
     // We are unsubscribed from the remote Participant's audio Track. We will no longer receive the
     // remote Participant's audio.
     
-    [self logMessage:[NSString stringWithFormat:@"Unsubscribed from audio track for Participant %@", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Unsubscribed from %@ audio track for Participant %@",
+                      publication.trackName, participant.identity]];
 }
 
 - (void)subscribedToDataTrack:(TVIRemoteDataTrack *)dataTrack
@@ -488,7 +506,8 @@
     // We are subscribed to the remote Participant's data Track. We will start receiving the
     // remote Participant's data messages now.
 
-    [self logMessage:[NSString stringWithFormat:@"Subscribed to data track for Participant %@", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Subscribed to %@ data track for Participant %@",
+                      publication.trackName, participant.identity]];
 
     dataTrack.delegate = self;
 }
@@ -500,27 +519,32 @@
     // We are unsubscribed from the remote Participant's data Track. We will no longer receive the
     // remote Participant's data messages.
 
-    [self logMessage:[NSString stringWithFormat:@"Unsubscribed from data track for Participant %@", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Unsubscribed from %@ data track for Participant %@",
+                      publication.trackName, participant.identity]];
 }
 
 - (void)remoteParticipant:(TVIRemoteParticipant *)participant
         enabledVideoTrack:(TVIRemoteVideoTrackPublication *)publication {
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ enabled video track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ enabled %@ video track.",
+                      participant.identity, publication.trackName]];
 }
 
 - (void)remoteParticipant:(TVIRemoteParticipant *)participant
        disabledVideoTrack:(TVIRemoteVideoTrackPublication *)publication {
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ disabled video track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ disabled %@ video track.",
+                      participant.identity, publication.trackName]];
 }
 
 - (void)remoteParticipant:(TVIRemoteParticipant *)participant
         enabledAudioTrack:(TVIRemoteAudioTrackPublication *)publication {
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ enabled audio track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ enabled %@ audio track.",
+                      participant.identity, publication.trackName]];
 }
 
 - (void)remoteParticipant:(TVIRemoteParticipant *)participant
        disabledAudioTrack:(TVIRemoteAudioTrackPublication *)publication {
-    [self logMessage:[NSString stringWithFormat:@"Participant %@ disabled audio track.", participant.identity]];
+    [self logMessage:[NSString stringWithFormat:@"Participant %@ disabled %@ audio track.",
+                      participant.identity, publication.trackName]];
 }
 
 #pragma mark - TVIRemoteDataTrackDelegate
