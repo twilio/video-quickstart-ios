d26d149d4bbe508f9a3e0964e554514b3715d3fd
Chris Eagleston <ceagleston@twilio.com>
2017-11-03 18:25:43 -0700

    Add skeleton ViewController.

--- AudioSinkExample/ViewController.swift
+++ AudioSinkExample/ViewController.swift
@@ -5,13 +5,40 @@
 //  Copyright © 2017 Twilio Inc. All rights reserved.
 //
 
+import TwilioVideo
 import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: View Controller Members
+
+    // Configure access token manually for testing, if desired! Create one manually in the console
+    // at https://www.twilio.com/user/account/video/dev-tools/testing-tools
+    var accessToken = "TWILIO_ACCESS_TOKEN"
+
+    // Configure remote URL to fetch token from
+    var tokenUrl = "http://localhost:8000/token.php"
+
+    // Video SDK components
+    var room: TVIRoom?
+    var localAudioTrack: TVILocalAudioTrack!
+    var localVideoTrack: TVILocalVideoTrack!
+
+    // MARK: UI Element Outlets and handles
+
+    @IBOutlet weak var connectButton: UIButton!
+    @IBOutlet weak var disconnectButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var roomTextField: UITextField!
+    @IBOutlet weak var roomLine: UIView!
+    @IBOutlet weak var roomLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.disconnectButton.isHidden = true
+        self.roomTextField.autocapitalizationType = .none
+        self.roomTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,6 +46,109 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: IBActions
+    @IBAction func connect(sender: AnyObject) {
+        // Configure access token either from server or manually.
+        // If the default wasn't changed, try fetching from server.
+        if (accessToken == "TWILIO_ACCESS_TOKEN") {
+            do {
+                accessToken = try TokenUtils.fetchToken(url: tokenUrl)
+            } catch {
+                let message = "Failed to fetch access token"
+                logMessage(messageText: message)
+                return
+            }
+        }
+
+        // Preparing the connect options with the access token that we fetched (or hardcoded).
+        let connectOptions = TVIConnectOptions.init(token: accessToken) { (builder) in
+
+            // The name of the Room where the Client will attempt to connect to. Please note that if you pass an empty
+            // Room `name`, the Client will create one for you. You can get the name or sid from any connected Room.
+            builder.roomName = self.roomTextField.text
+        }
+
+        // Connect to the Room using the options we provided.
+        room = TwilioVideo.connect(with: connectOptions, delegate: self)
+
+        logMessage(messageText: "Attempting to connect to room \(String(describing: self.roomTextField.text))")
+
+        self.showRoomUI(inRoom: true)
+        self.dismissKeyboard()
+    }
+
+    @IBAction func disconnect(sender: AnyObject) {
+        if let room = self.room {
+            room.disconnect()
+            logMessage(messageText: "Attempting to disconnect from room \(room.name)")
+        }
+    }
+
+    // Update our UI based upon if we are in a Room or not
+    func showRoomUI(inRoom: Bool) {
+        self.connectButton.isHidden = inRoom
+        self.roomTextField.isHidden = inRoom
+        self.roomLine.isHidden = inRoom
+        self.roomLabel.isHidden = inRoom
+        self.disconnectButton.isHidden = !inRoom
+        UIApplication.shared.isIdleTimerDisabled = inRoom
+    }
+
+    func dismissKeyboard() {
+        if (self.roomTextField.isFirstResponder) {
+            self.roomTextField.resignFirstResponder()
+        }
+    }
+
+    func logMessage(messageText: String) {
+        messageLabel.text = messageText
+    }
 
 }
+
+// MARK: UITextFieldDelegate
+extension ViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.connect(sender: textField)
+        return true
+    }
+}
+
+// MARK: TVIRoomDelegate
+extension ViewController : TVIRoomDelegate {
+    func didConnect(to room: TVIRoom) {
+
+        for remoteParticipant in room.remoteParticipants {
+//            remoteParticipant.delegate = self
+        }
+
+        logMessage(messageText: "Connected to room \(room.name) as \(String(describing: room.localParticipant?.identity))")
+    }
+
+    func room(_ room: TVIRoom, didDisconnectWithError error: Error?) {
+        logMessage(messageText: "Disconncted from room \(room.name), error = \(String(describing: error))")
+
+        self.room = nil
+
+        self.showRoomUI(inRoom: false)
+    }
+
+    func room(_ room: TVIRoom, didFailToConnectWithError error: Error) {
+        logMessage(messageText: "Failed to connect to room with error: \(error.localizedDescription)")
+        self.room = nil
+
+        self.showRoomUI(inRoom: false)
+    }
+
+    func room(_ room: TVIRoom, participantDidConnect participant: TVIRemoteParticipant) {
+//        participant.delegate = self
+
+        logMessage(messageText: "Participant \(participant.identity) connected with \(participant.remoteDataTracks.count) data, \(participant.remoteAudioTracks.count) audio and \(participant.remoteVideoTracks.count) video tracks")
+    }
+
+    func room(_ room: TVIRoom, participantDidDisconnect participant: TVIRemoteParticipant) {
+        logMessage(messageText: "Room \(room.name), Participant \(participant.identity) disconnected")
+    }
+}
+
 
