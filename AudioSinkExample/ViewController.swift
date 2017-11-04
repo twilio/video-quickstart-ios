bf04c2202e59231f607eef7dcd5e4b32e41fc919
Chris Eagleston <ceagleston@twilio.com>
2017-11-03 17:04:12 -0700

    Add single view template for AudioSinkExample.

---
+++ AudioSinkExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioSinkExample
+//
+//  Created by Chris Eagleston on 11/3/17.
+//  Copyright © 2017 Twilio Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
