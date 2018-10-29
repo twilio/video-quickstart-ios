6c9600987364ebec52115c165df8672a01d017f1
Chris Eagleston <ceagleston@twilio.com>
2018-10-29 16:48:39 -0700

    Add new project with empty template.

---
+++ CoViewingExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoViewingExample
+//
+//  Copyright © 2018 Twilio Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
