1c22a0dceb94a49a5e98bd4ea8c7e7411f1cf2de
Ryan C. Payne <rpayne@twilio.com>
2019-03-07 10:14:54 -0500

    Added MultiPartyViewController

---
+++ MultiPartyExample/MultiPartyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MultiPartyViewController.swift
+//  MultiPartyExample
+//
+//  Copyright © 2019 Twilio, Inc. All rights reserved.
+//
+
+import UIKit
+
+class MultiPartyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
