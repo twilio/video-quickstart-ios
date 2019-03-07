0ca4348939954d4996b795c4cfdcd2e891ff5f8f
Ryan C. Payne <rpayne@twilio.com>
2019-03-07 10:13:15 -0500

    Added message label back for on screen logging

--- MultiPartyExample/MainViewController.swift
+++ MultiPartyExample/MainViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 class MainViewController: UIViewController {
 
     // MARK: View Controller Members
+
+    // Configure access token manually for testing, if desired! Create one manually in the console
+    // at https://www.twilio.com/console/video/runtime/testing-tools
+    var accessToken = "TWILIO_ACCESS_TOKEN"
+
+    // Configure remote URL to fetch token from
+    var tokenUrl = "http://localhost:8000/token.php"
+
+    // MARK: UI Element Outlets and handles
     @IBOutlet weak var connectButton: UIButton!
     @IBOutlet weak var roomTextField: UITextField!
     @IBOutlet weak var roomLine: UIView!
     @IBOutlet weak var roomLabel: UILabel!
-    
+    @IBOutlet weak var messageLabel: UILabel!
+
+    // MARK: UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "MultiPartyExample"
+        self.messageLabel.adjustsFontSizeToFitWidth = true;
+        self.messageLabel.minimumScaleFactor = 0.75;
 
         if let navigationController = self.navigationController {
             navigationController.navigationBar.barTintColor = UIColor.init(red: 226.0/255.0,
@@ -41,11 +54,28 @@ class MainViewController: UIViewController {
             self.roomTextField.resignFirstResponder()
         }
     }
-    
+
+    func logMessage(messageText: String) {
+        NSLog(messageText)
+        messageLabel.text = messageText
+    }
+
     @IBAction func connect(_ sender: Any) {
         guard let roomName = roomTextField.text, !roomName.isEmpty else {
             self.roomTextField.becomeFirstResponder()
             return
+        }
+
+        // Configure access token either from server or manually.
+        // If the default wasn't changed, try fetching from server.
+        if (accessToken == "TWILIO_ACCESS_TOKEN") {
+            do {
+                accessToken = try TokenUtils.fetchToken(url: tokenUrl)
+            } catch {
+                let message = "Failed to fetch access token"
+                logMessage(messageText: message)
+                return
+            }
         }
 
         print("Connecting to room \(roomName)")
