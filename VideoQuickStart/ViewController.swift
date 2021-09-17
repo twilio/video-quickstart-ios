1ac19516d40dc7b0f83ebfb37d62c232bf33dd16
Dipankar Das <didas@twilio.com>
2021-09-17 12:13:05 -0700

    Audio sink example

--- VideoQuickStart/ViewController.swift
+++ VideoQuickStart/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     var accessToken = "TWILIO_ACCESS_TOKEN"
   
     // Configure remote URL to fetch token from
-    var tokenUrl = "http://localhost:8000/token.php"
+    let tokenUrl = "http://localhost:8000/token.php"
     
     // Video SDK components
     var room: Room?
